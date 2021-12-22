d77a5c2d7b93cea4a5a2ee5ad8805297345789e5
hazedrops <hazedrops@hotmail.com>
2021-12-21 23:10:22 -0800

    Added Router to link additional page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+  return (
+    <Card className="about">
+      <h1>About This Project</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, nihil odio doloremque voluptas voluptatum ullam ratione eos ea expedita velit.</p>
+      <p>Version: 1.0.0</p>
+
+      <p>
+        <a href="/">Back To Home</a>
+      </p>
+    </Card>
+  )
+}
+
+export default AboutPage
