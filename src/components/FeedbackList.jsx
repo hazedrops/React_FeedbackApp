b780d62a6d93520d88c74dd4d9f09ad302c57ba4
hazedrops <hazedrops@hotmail.com>
2021-12-21 16:49:25 -0800

    Added animation using Framer-Motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-import { computeHeadingLevel } from '@testing-library/react';
+import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types';
 import FeedbackItem from "./FeedbackItem"
 
@@ -7,23 +7,46 @@ function FeedbackList({ feedback, handleDelete }) {
     return <p>No Feedback Yet!</p>
   }
 
-  return (
+   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem 
-          key={item.id} 
-          item={item} 
-          handleDelete={handleDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div 
+            key={item.id}
+            initial={{opacity: 0}} 
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+          >
+            <FeedbackItem 
+              key={item.id} 
+              item={item} 
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+
+  // Version without the animation
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem 
+  //         key={item.id} 
+  //         item={item} 
+  //         handleDelete={handleDelete}
+  //       />
+  //     ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
     })
